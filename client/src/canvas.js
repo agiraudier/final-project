140fc044a6fbf83814a48ce24832956acc81fa72
Andrea Giraudier <andrea.giraudier@gmail.com>
2021-03-01 22:41:59 +0100

    Final project canvas line and rectangle tools updated

--- client/src/canvas.js
+++ client/src/canvas.js
@@ -1,9 +1,10 @@
 import { useState, useRef, useEffect } from "react";
 
 export function Canvas() {
-    //const [elem, setElem] = useState([]);
+    //const [color, setColor] = useState({});
     const [elemType, setElemType] = useState("line");
     const [isDrawing, setIsDrawing] = useState(false);
+    const [firstPoint, setFirstPoint] = useState({ x: null, y: null });
     const canvasRef = useRef(null);
     const contextRef = useRef(null);
 
@@ -15,16 +16,12 @@ export function Canvas() {
         context.lineWidth = 2;
     }, []);
 
-    const drawLine = (x1, y1, x2, y2) => {
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(x1, y1);
-        contextRef.current.lineTo(x2, y2);
-        contextRef.current.stroke();
-        contextRef.current.closePath();
-    };
-
     const handleMouseDown = ({ nativeEvent }) => {
         setIsDrawing(true);
+        setFirstPoint({
+            x: nativeEvent.offsetX,
+            y: nativeEvent.offsetY,
+        });
         if (elemType == "free") {
             const x = nativeEvent.offsetX;
             const y = nativeEvent.offsetY;
@@ -35,15 +32,13 @@ export function Canvas() {
         } else if (elemType == "line") {
             const x = nativeEvent.offsetX;
             const y = nativeEvent.offsetY;
+            contextRef.current.beginPath();
+            contextRef.current.moveTo(x, y);
+            //contextRef.current.lineTo(x, y);
 
             console.log("handlemousedown line: ", x, y);
-
-            contextRef.current.clearRect(
-                0,
-                0,
-                canvasRef.width,
-                canvasRef.height
-            );
+        } else if (elemType == "rectangle") {
+            console.log("handle mouse down rectangle: ");
         }
     };
 
@@ -55,14 +50,14 @@ export function Canvas() {
         if (elemType == "free") {
             const x = nativeEvent.offsetX;
             const y = nativeEvent.offsetY;
-
             contextRef.current.lineTo(x, y);
             contextRef.current.stroke();
         } else if (elemType == "line") {
             const x = nativeEvent.offsetX;
             const y = nativeEvent.offsetY;
-
-            console.log("handlemove line: ", x, y);
+            console.log(x, y);
+        } else if (elemType == "rectangle") {
+            console.log("moving rectangle");
         }
     };
 
@@ -75,57 +70,69 @@ export function Canvas() {
             const x2 = nativeEvent.offsetX;
             const y2 = nativeEvent.offsetY;
             console.log("handleup line: ", x2, y2);
-            drawLine(x2, y2);
+
+            contextRef.current.lineTo(x2, y2);
+            contextRef.current.stroke();
+            contextRef.current.closePath();
+        } else if (elemType == "rectangle") {
+            const x1 = firstPoint.x;
+            const y1 = firstPoint.y;
+            console.log("handlemouse up rectangle get from state: ", x1, y1);
+
+            const x2 = nativeEvent.offsetX;
+            const y2 = nativeEvent.offsetY;
+
+            console.log("handlemouse up rectangle current: ", x2, y2);
+
+            const width = Math.abs(x2 - x1);
+            const height = Math.abs(y2 - y1);
+
+            //need 4 states to solve bug in drawing direction
+
+            if (x2 >= x1 && y2 >= y1) {
+                contextRef.current.strokeRect(x1, y1, width, height);
+            } else if (x2 >= x1 && y2 < y1) {
+                contextRef.current.strokeRect(x1, y1 - height, width, height);
+            } else if (x2 < x1 && y2 < y1) {
+                contextRef.current.strokeRect(x2, y2, width, height);
+            } else {
+                contextRef.current.strokeRect(x1 - width, y1, width, height);
+            }
+
+            setFirstPoint({
+                x: null,
+                y: null,
+            });
         }
     };
 
-    const handleMouseOut = () => {
+    const handleMouseOut = ({ nativeEvent }) => {
         setIsDrawing(false);
         if (elemType == "free") {
             contextRef.current.closePath();
         } else if (elemType == "line") {
+            const x2 = nativeEvent.offsetX;
+            const y2 = nativeEvent.offsetY;
+            console.log("handleup line: ", x2, y2);
+            // drawLine(x2, y2);
+            //contextRef.current.lineTo(x2, y2);
+            //contextRef.current.closePath();
+
             contextRef.current.closePath();
+            contextRef.current.stroke();
         }
     };
-
-    /*const canvas = document.getElementById("signature");
-const context = canvas.getContext("2d");
-const form = document.getElementById("form");
-const inputArea = document.getElementById("canvas");
-
-let isDrawing = false;
-let x = 0;
-let y = 0;
-
-let drawLine = (e) => {
-    if (!isDrawing) return;
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.lineWidth = 2;
-    //context.moveTo(x1, y1);
-    //context.lineTo(x2, y2);
-    context.moveTo(x, y);
-    context.lineTo(e.offsetX, e.offsetY);
-    context.stroke();
-    context.closePath();
-    x = e.offsetX;
-    y = e.offsetY;
-};
-
-canvas.addEventListener("mousedown", (e) => {
-    isDrawing = true;
-    x = e.offsetX;
-    y = e.offsetY;
-});
-
-canvas.addEventListener("mousemove", drawLine);
-canvas.addEventListener("mouseup", () => (isDrawing = false));
-canvas.addEventListener("mouseout", () => (isDrawing = false));
-*/
 
     return (
         <div>
             <div>
+                <input
+                    type="radio"
+                    id="free"
+                    checked={elemType == "free"}
+                    onChange={() => setElemType("free")}
+                ></input>
+                <label htmlFor="free">Free</label>
                 <input
                     type="radio"
                     id="line"
@@ -140,13 +147,7 @@ canvas.addEventListener("mouseout", () => (isDrawing = false));
                     onChange={() => setElemType("rectangle")}
                 ></input>
                 <label htmlFor="rectangle">Rectangle</label>
-                <input
-                    type="radio"
-                    id="free"
-                    checked={elemType == "free"}
-                    onChange={() => setElemType("free")}
-                ></input>
-                <label htmlFor="free">Free</label>
+
                 <input
                     type="radio"
                     id="circle"
@@ -154,16 +155,22 @@ canvas.addEventListener("mouseout", () => (isDrawing = false));
                     onChange={() => setElemType("circle")}
                 ></input>
                 <label htmlFor="circle">Circle</label>
+                <input
+                    type="color"
+                    id="color"
+                    checked={elemType == "color"}
+                    onChange={() => setElemType("color")}
+                ></input>
             </div>
             <canvas
                 id="canvas"
-                width="300"
-                height="300"
+                width="600"
+                height="600"
                 ref={canvasRef}
                 onMouseDown={(e) => handleMouseDown(e)}
                 onMouseMove={(e) => handleMouseMove(e)}
                 onMouseUp={(e) => handleMouseUp(e)}
-                onMouseOut={() => handleMouseOut()}
+                onMouseOut={(e) => handleMouseOut(e)}
             ></canvas>
         </div>
     );
